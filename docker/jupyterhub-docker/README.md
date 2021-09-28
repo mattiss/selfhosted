6ee6f9d50dfe317f03df9e44ad735b71a39bb355
MatTiss <matthieu.tisserand@polytechnique.org>
2021-09-28 13:56:47 +0200

    JupyterHub (Multiusers Notebooks and more) docker-compose

---
+++ docker/jupyterhub-docker/README.md
@@ -0,0 +1,3 @@
+## Learn more
+This deployment is described in depth in [this blog
+post](https://opendreamkit.org/2018/10/17/jupyterhub-docker/).
---
+++ docker/jupyterhub-docker/jupyterhub/jupyterhub_config.py
@@ -0,0 +1,107 @@
+# JupyterHub configuration
+#
+## If you update this file, do not forget to delete the `jupyterhub_data` volume before restarting the jupyterhub service:
+##
+##     docker volume rm jupyterhub_jupyterhub_data
+##
+## or, if you changed the COMPOSE_PROJECT_NAME to <name>:
+##
+##    docker volume rm <name>_jupyterhub_data
+##
+
+import os
+import sys
+
+# Enable JupyterLab interface if enabled.
+c.Spawner.environment = {}
+if os.environ.get('JUPYTERHUB_ENABLE_LAB', 'false').lower() in ['true', 'yes', 'y', '1']:
+    c.Spawner.environment.update(dict(JUPYTER_ENABLE_LAB='true'))
+    c.Spawner.cmd=["jupyter-labhub"]
+    c.Spawner.default_url = '/lab'
+
+# Setup location for customised template files.
+c.JupyterHub.template_paths = ['/opt/app-root/src/templates']
+
+# Configure KeyCloak as authentication provider.
+## Generic
+c.JupyterHub.admin_access = True
+
+
+public_hostname = os.environ.get('PUBLIC_HOST_NAME')
+keycloak_hostname = os.environ.get('KEYCLOAK_HOST_NAME')
+keycloak_realm = os.environ.get('KEYCLOAK_REALM')
+keycloak_account_url = 'https://%s/auth/realms/%s/account' % (
+        keycloak_hostname, keycloak_realm)
+
+with open('templates/vars.html', 'w') as fp:
+    fp.write('{%% set keycloak_account_url = "%s" %%}' % keycloak_account_url)
+
+os.environ['OAUTH2_TOKEN_URL'] = 'https://%s/auth/realms/%s/protocol/openid-connect/token' % (keycloak_hostname, keycloak_realm)
+os.environ['OAUTH2_AUTHORIZE_URL'] = 'https://%s/auth/realms/%s/protocol/openid-connect/auth' % (keycloak_hostname, keycloak_realm)
+os.environ['OAUTH2_USERDATA_URL'] = 'https://%s/auth/realms/%s/protocol/openid-connect/userinfo' % (keycloak_hostname, keycloak_realm)
+
+os.environ['OAUTH2_TLS_VERIFY'] = '0'
+os.environ['OAUTH_TLS_VERIFY'] = '0'
+
+os.environ['OAUTH2_USERNAME_KEY'] = 'preferred_username'
+
+from oauthenticator.generic import GenericOAuthenticator
+c.JupyterHub.authenticator_class = GenericOAuthenticator
+
+c.OAuthenticator.login_service = "KeyCloak"
+c.OAuthenticator.oauth_callback_url = 'https://%s/hub/oauth_callback' % public_hostname
+
+c.OAuthenticator.client_id = os.environ.get('OAUTH_CLIENT_ID')
+c.OAuthenticator.client_secret = os.environ.get('OAUTH_CLIENT_SECRET')
+
+c.OAuthenticator.tls_verify = False
+c.OAuthenticator.username_key = "preferred_username"
+
+# Populate admin users and use white list from config maps.
+if os.path.exists('/opt/app-root/configs/admin_users.txt'):
+    with open('/opt/app-root/configs/admin_users.txt') as fp:
+        content = fp.read().strip()
+        if content:
+            c.Authenticator.admin_users = set(content.split())
+
+if os.path.exists('/opt/app-root/configs/user_whitelist.txt'):
+    with open('/opt/app-root/configs/user_whitelist.txt') as fp:
+        content = fp.read().strip()
+        if content:
+            c.Authenticator.whitelist = set(content.split())
+
+## Docker spawner
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+
+c.DockerSpawner.allowed_images = {
+   "datascience"     : "jupyter/datascience-notebook",
+   "minimal"     : "jupyter/minimal-notebook:latest",
+   "all-spark"   : "jupyter/all-spark-notebook:latest",
+   "atoti"   : "atoti/atoti",
+   "custom" : os.environ['DOCKER_JUPYTER_CONTAINER'],
+}
+
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+# See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
+# user data persistence
+# see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+
+# Other stuff
+c.Spawner.cpu_limit = 1
+c.Spawner.mem_limit = '10G'
+c.DockerSpawner.http_timeout = 100
+c.DockerSpawner.start_timeout = 200
+
+## Services
+c.JupyterHub.services = [
+    {
+        'name': 'idle-culler',
+        'admin': True,
+        'command': [sys.executable, '-m', 'jupyterhub_idle_culler', '--timeout=3600'],
+    }
+]
